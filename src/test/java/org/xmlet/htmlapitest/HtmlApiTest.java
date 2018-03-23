61f6677ed522d9cfc4d0b06e274a0dd83dbde779
lcduarte <lois_duarte@msn.com>
2018-03-23 14:01:17 +0000

    * Changes tests;

--- src/test/java/org/xmlet/htmlapitest/HtmlApiTest.java
+++ src/test/java/org/xmlet/htmlapitest/HtmlApiTest.java
@@ -1,20 +1,31 @@
 package org.xmlet.htmlapitest;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.xmlet.htmlapi.*;
 import org.xmlet.htmlapitest.utils.CustomVisitor;
 import org.xmlet.htmlapitest.utils.Student;
-import org.junit.Assert;
-import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
+import java.lang.Object;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class HtmlApiTest {
+
+    static final String PACKAGE = "org.xmlet.htmlapi";
 
     @Test
     public void testGeneratedClassesIntegrity() throws Exception {
@@ -24,8 +35,8 @@ public class HtmlApiTest {
                 .meta().attrCharset("UTF-8").º()
                 .title()
                     .text("Title").º()
-                .link().attrType(Enumtype.TEXT_CSS).attrHref("/assets/images/favicon.png").º()
-                .link().attrType(Enumtype.TEXT_CSS).attrHref("/assets/styles/main.css").º().º()
+                .link().attrType(EnumTypeContentType.TEXT_CSS).attrHref("/assets/images/favicon.png").º()
+                .link().attrType(EnumTypeContentType.TEXT_CSS).attrHref("/assets/styles/main.css").º().º()
             .body().attrClass("clear")
                 .div()
                     .header()
@@ -47,8 +58,8 @@ public class HtmlApiTest {
                                                             .meta().attrCharset("UTF-8").º()
                                                             .title()
                                                                  .text("Title").º()
-                                                            .link().attrType(Enumtype.TEXT_CSS).attrHref("/assets/images/favicon.png").º()
-                                                            .link().attrType(Enumtype.TEXT_CSS).attrHref("/assets/styles/main.css").º().º()
+                                                            .link().attrType(EnumTypeContentType.TEXT_CSS).attrHref("/assets/images/favicon.png").º()
+                                                            .link().attrType(EnumTypeContentType.TEXT_CSS).attrHref("/assets/styles/main.css").º().º()
                                                             .body().attrClass("clear")
                                                                 .div()
                                                                     .header()
@@ -81,7 +92,7 @@ public class HtmlApiTest {
      */
     @Test
     public void testRestrictionSuccess(){
-        new AttrRel(Enumrel.HELP);
+        new AttrRelEnumRelLinkType(EnumRelLinkType.HELP);
     }
 
     /**
@@ -173,7 +184,7 @@ public class HtmlApiTest {
 
     @Test
     public void testAttributeName(){
-        Assert.assertEquals("class", new AttrClass(null).getName());
+        Assert.assertEquals("class", new AttrClassString(null).getName());
     }
 
     @Test
@@ -193,9 +204,164 @@ public class HtmlApiTest {
     @Test
     public void testTextExceptions(){
         try {
-            new Text<>(new Html(), "dummy").addAttr(new AttrHref(""));
+            new Text<>(new Html(), "dummy").addAttr(new AttrHrefString(""));
             Assert.fail();
         } catch (UnsupportedOperationException ignored){}
+    }
+
+    @Test
+    public void testAllElements() throws MalformedURLException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        URL targetUrl = Element.class.getClassLoader().getResource("");
+
+        Assert.assertNotNull(targetUrl);
+
+        String target = targetUrl.getPath().replaceAll("test-", "") + "org/xmlet/htmlapi";
+
+        File generatedObjectsFolder = new File(target);
+        File[] generatedFiles = generatedObjectsFolder.listFiles();
+
+        assert generatedFiles != null;
+
+        URLClassLoader ucl = new URLClassLoader(new URL[]{ new URL("file://" + target )});
+
+        for (File generatedFile : generatedFiles) {
+            CustomVisitor visitor = new CustomVisitor();
+
+            if (generatedFile.getName().endsWith(".class")){
+                String absolutePath = generatedFile.getAbsolutePath();
+
+                String className = absolutePath.substring(absolutePath.lastIndexOf('\\') + 1, absolutePath.indexOf(".class"));
+
+                Class<?> klass = ucl.loadClass(PACKAGE + "." + className);
+
+                if (Modifier.isAbstract(klass.getModifiers()) || Modifier.isInterface(klass.getModifiers()) || klass.isEnum() || klass.getName().contains("Text")){
+                    continue;
+                }
+
+                if (AbstractElement.class.isAssignableFrom(klass)){
+                    Constructor ctor1 = klass.getConstructor();
+                    Constructor ctor2 = klass.getConstructor(String.class);
+                    Constructor ctor3 = klass.getConstructor(Element.class);
+                    Constructor ctor4 = klass.getConstructor(Element.class, String.class);
+
+                    Object elementInstance = ctor1.newInstance();
+                    ctor2.newInstance("name");
+                    ctor3.newInstance(new Object[]{null});
+                    ctor4.newInstance(null, "name");
+
+                    Method[] methods = klass.getMethods();
+
+                    for (Method method : methods) {
+                        if (method.getParameterCount() == 0 && Element.class.isAssignableFrom(method.getReturnType())){
+                            method.invoke(elementInstance);
+                        }
+
+                        if (method.getParameterCount() == 1 && method.getName().startsWith("attr")){
+                            Class<?> paramType = method.getParameterTypes()[0];
+                            if (!paramType.isEnum()){
+                                if (paramType.equals(String.class) || paramType.equals(Object.class)){
+                                    method.invoke(elementInstance, new Object[]{null});
+                                }
+
+                                if (paramType.equals(Short.class)){
+                                    method.invoke(elementInstance, (short) 0);
+                                }
+
+                                if (paramType.equals(Double.class)){
+                                    method.invoke(elementInstance, (double) 0);
+                                }
+
+                                if (paramType.equals(Integer.class)){
+                                    method.invoke(elementInstance, 0);
+                                }
+
+                                if (paramType.equals(Float.class)){
+                                    method.invoke(elementInstance, 0f);
+                                }
+
+                                if (paramType.equals(Boolean.class)){
+                                    method.invoke(elementInstance, false);
+                                }
+                            }
+                        }
+                    }
+
+                    ((Element)elementInstance).accept(visitor);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testEnums(){
+        new AttrRelEnumRelLinkType(EnumRelLinkType.HELP);
+        new AttrTargetEnumTargetBrowsingContext(EnumTargetBrowsingContext._BLANK);
+        new AttrFormtargetEnumFormtargetBrowsingContext(EnumFormtargetBrowsingContext._BLANK);
+        new AttrMediaEnumMediaMediaType(EnumMediaMediaType.ALL);
+        new AttrAsyncEnumAsyncscript(EnumAsyncscript.ASYNC);
+        new AttrCheckedEnumCheckedcommand(EnumCheckedcommand.CHECKED);
+        new AttrControlsEnumControlsaudio(EnumControlsaudio.CONTROLS);
+        new AttrControlsEnumControlsvideo(EnumControlsvideo.CONTROLS);
+        new AttrAutofocusEnumAutofocusbutton(EnumAutofocusbutton.AUTOFOCUS);
+        new AttrAutofocusEnumAutofocuskeygen(EnumAutofocuskeygen.AUTOFOCUS);
+        new AttrAutofocusEnumAutofocusselect(EnumAutofocusselect.AUTOFOCUS);
+        new AttrAutofocusEnumAutofocustextarea(EnumAutofocustextarea.AUTOFOCUS);
+        new AttrAutobufferEnumAutobufferaudio(EnumAutobufferaudio.AUTOBUFFER);
+        new AttrAutobufferEnumAutobuffervideo(EnumAutobuffervideo.AUTOBUFFER);
+        new AttrAutocompleteEnumAutocompleteform(EnumAutocompleteform.ON);
+        new AttrAutoplayEnumAutoplayaudio(EnumAutoplayaudio.AUTOPLAY);
+        new AttrCheckedEnumCheckedinput(EnumCheckedinput.CHECKED);
+        new AttrDisabledEnumDisabledcommand(EnumDisabledcommand.DISABLED);
+        new AttrContenteditableEnumContenteditable(EnumContenteditable.FALSE);
+        new AttrDeferEnumDeferscript(EnumDeferscript.DEFER);
+        new AttrDirEnumDir(EnumDir.RTL);
+        new AttrDisabledEnumDisabledbutton(EnumDisabledbutton.AUTOFOCUS);
+        new AttrDisabledEnumDisabledcommand(EnumDisabledcommand.DISABLED);
+        new AttrDisabledEnumDisabledinput(EnumDisabledinput.DISABLED);
+        new AttrDisabledEnumDisabledkeygen(EnumDisabledkeygen.DISABLED);
+        new AttrDisabledEnumDisabledoptgroup(EnumDisabledoptgroup.DISABLED);
+        new AttrDisabledEnumDisabledoption(EnumDisabledoption.DISABLED);
+        new AttrDisabledEnumDisabledselect(EnumDisabledselect.AUTOFOCUS);
+        new AttrDisabledEnumDisabledtextarea(EnumDisabledtextarea.DISABLED);
+        new AttrDraggableEnumDraggable(EnumDraggable.AUTO);
+        new AttrEnctypeEnumEnctypeform(EnumEnctypeform.MULTIPART_FORM_DATA);
+        new AttrAutoplayEnumAutoplayvideo(EnumAutoplayvideo.AUTOPLAY);
+        new AttrFormenctypeEnumFormenctypebutton(EnumFormenctypebutton.APPLICATION_X_WWW_FORM_URLENCODED);
+        new AttrFormenctypeEnumFormenctypeinput(EnumFormenctypeinput.APPLICATION_X_WWW_FORM_URLENCODED);
+        new AttrFormmethodEnumFormmethodbutton(EnumFormmethodbutton.DELETE);
+        new AttrFormmethodEnumFormmethodinput(EnumFormmethodinput.DELETE);
+        new AttrFormnovalidateEnumFormnovalidatebutton(EnumFormnovalidatebutton.FORMNOVALIDATE);
+        new AttrFormnovalidateEnumFormnovalidateinput(EnumFormnovalidateinput.FORMNOVALIDATE);
+        new AttrHiddenEnumHidden(EnumHidden.HIDDEN);
+        new AttrHttpequivEnumHttpequivmeta(EnumHttpequivmeta.REFRESH);
+        new AttrIsmapEnumIsmapimg(EnumIsmapimg.ISMAP);
+        new AttrKeytypeEnumKeytypekeygen(EnumKeytypekeygen.RSA);
+        new AttrLoopEnumLoopaudio(EnumLoopaudio.LOOP);
+        new AttrLoopEnumLoopvideo(EnumLoopvideo.LOOP);
+        new AttrMethodEnumMethodform(EnumMethodform.DELETE);
+        new AttrMultipleEnumMultipleselect(EnumMultipleselect.MULTIPLE);
+        new AttrNameEnumNameBrowsingContext(EnumNameBrowsingContext._BLANK);
+        new AttrNovalidateEnumNovalidateform(EnumNovalidateform.NOVALIDATE);
+        new AttrOpenEnumOpendetails(EnumOpendetails.OPEN);
+        new AttrReadonlyEnumReadonlytextarea(EnumReadonlytextarea.READONLY);
+        new AttrRequiredEnumRequiredtextarea(EnumRequiredtextarea.REQUIRED);
+        new AttrReversedEnumReversedol(EnumReversedol.REVERSED);
+        new AttrRunatEnumRunat(EnumRunat.SERVER);
+        new AttrSandboxEnumSandboxiframe(EnumSandboxiframe.ALLOW_FORMS);
+        new AttrScopedEnumScopedstyle(EnumScopedstyle.SCOPED);
+        new AttrScopeEnumScopeth(EnumScopeth.COL);
+        new AttrSeamlessEnumSeamlessiframe(EnumSeamlessiframe.SEAMLESS);
+        new AttrSelectedEnumSelectedoption(EnumSelectedoption.SELECTED);
+        new AttrShapeEnumShapearea(EnumShapearea.CIRCLE);
+        new AttrSpellcheckEnumSpellcheck(EnumSpellcheck.FALSE);
+        new AttrTypeEnumTypebutton(EnumTypebutton.BUTTON);
+        new AttrTypeEnumTypecommand(EnumTypecommand.CHECKBOX);
+        new AttrTypeEnumTypeinput(EnumTypeinput.BUTTON);
+        new AttrTypeEnumTypemenu(EnumTypemenu.CONTEXT);
+        new AttrTypeEnumTypescript(EnumTypescript.TEXT_ECMASCRIPT);
+        new AttrTypeEnumTypeSimpleContentType(EnumTypeSimpleContentType.TEXT_ASA);
+        new AttrTypeEnumTypestyle(EnumTypestyle.TEXT_CSS);
+        new AttrWrapEnumWraptextarea(EnumWraptextarea.HARD);
     }
 
     private boolean customVisitPrintAssert(CustomVisitor customVisitor, Html rootDoc, String expected){
@@ -219,5 +385,4 @@ public class HtmlApiTest {
 
         return result;
     }
-
 }
